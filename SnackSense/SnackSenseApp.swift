45e387f350251377b7cd917fc10822d56eeb2e6d
Tom <8527295+OfficialTomJ@users.noreply.github.com>
2025-04-22 15:37:23 +1000

    first commit

---
+++ SnackSense/SnackSenseApp.swift
@@ -0,0 +1,32 @@
+//
+//  SnackSenseApp.swift
+//  SnackSense
+//
+//  Created by Tom on 22/4/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SnackSenseApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SnackSenseTests/SnackSenseTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnackSenseTests.swift
+//  SnackSenseTests
+//
+//  Created by Tom on 22/4/2025.
+//
+
+import Testing
+@testable import SnackSense
+
+struct SnackSenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
